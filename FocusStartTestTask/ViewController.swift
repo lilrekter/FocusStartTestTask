06864aa29f97010ab9d29e919cd1ffcc27b0c4b7
Vitaliy B <lilrekter@mail.ru>
2024-03-10 18:29:24 +0300

    Initial commit

---
+++ FocusStartTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FocusStartTestTask
+//
+//  Created by Vitaly on 10.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
